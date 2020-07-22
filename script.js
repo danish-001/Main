c201d7c6c23dab647f15a6cfafeb718f8ba5ec0f
Danish <sheikhmdanish.001gmail.com>
2020-07-22 18:45:14 +0530

    made the calculator work just fine

--- script.js
+++ script.js
@@ -9,31 +9,39 @@ class Calculator {
 
   // To clear the screen
   clear() {
-    this.previousOperand = '';
-    this.currentOperand = '';
-    this.operation = undefined;
+    this.previousOperand = 0;
+    this.currentOperand = 0;
+    this.operation = '';
   }
 
   // To delete the digits one by one
   delete() {
-    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    this.currentOperand = this.currentOperand.toString().length > 0 ? this.currentOperand.toString().slice(0, -1) : 0 
   }
  
   // To add a number to the screen everytime you click in the numbers
   appendNumber(number) {
-    if (number === '.' && this.currentOperand.includes('.')) return
-    this.currentOperand = this.currentOperand.toString() + number.toString()
+    
+    if (number === '.' && this.currentOperand.includes('.')) return 0
+    if (this.currentOperand == 0) {
+      this.currentOperand = `${number}`
+      console.log(this.currentOperand)
+      return true
+    }
+    this.currentOperand = `${this.currentOperand}${number}`
+    console.log(this.currentOperand)
   }
 
   // Yeah you guessed it right
   chooseOperations(operation) {
-    if (this.currentOperand === "") return
+    if (this.currentOperand === "") return ''
+    
     if (this.previousOperand !== "") { 
       this.compute();
     }
     this.operation = operation
     this.previousOperand = this.currentOperand // assigning output value to history value
-    this.currentOperand = ''
+    this.currentOperand = 0
   }
 
   // Take values and compute a single value
@@ -42,11 +50,11 @@ class Calculator {
     let computation;
 
     // Converting both strings to a number
-    const prev = parseFloat(this.previousOperand);
-    const current = parseFloat(this.currentOperand);
+    const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
 
     // If there is nothing in the screen
-    if (isNaN(prev) || isNaN(current)) return
+    if (isNaN(prev) || isNaN(current)) return 0
     
     switch (this.operation) {
       case '+':
@@ -62,25 +70,27 @@ class Calculator {
         computation = prev / current;
         break
       default:
-        return
+        return 0
     }
 
     // To clear out the screen
-    this.currentOperand = computation
-    this.operation = undefined
     this.previousOperand = ''
+    this.operation = ''
+    this.currentOperand = computation
+    
   }
 
   getDisplayNumber(number) {
 
-    // Converting the number to a string
+    // Converting the number to a string so we can split that string
     const stringNumber = number.toString()
 
     // To split the number into an array where integer digits 
-    // and decimal digits will be separeted by a.
-    const integerDigits = parseFloat(stringNumber.split('.'), [0])
+    // and decimal digits will be separeted by a .
+    let integerDigits = parseFloat(stringNumber.split('.')[0])
     const decimalDigits = stringNumber.split('.')[1]
 
+    // To overcome the problem of not having the . in the beginning
     let integerDisplay
     if (isNaN(integerDigits)) {
       integerDigits = '';
@@ -97,26 +107,27 @@ class Calculator {
 
   // To let us update the display
   updateDisplay() {
+
     this.currentOperandText.innerText = this.getDisplayNumber(this.currentOperand)
     if (this.operation != null) {
-      this.previousOperandText.innerText = `${this.thisgetDisplayNumber(this.previousOperand)} ${this.operation}`;
+      this.previousOperandText.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
     } else {
-      this.previousOperandText.innerText = ''
+      this.previousOperandText.innerText = 0
     }
   }
 }
 
 // 1.
 // Get all the data and asign it to a constant
-const numberButtons = document.querySelectorAll('.number');
+const numberButtons = document.querySelectorAll('.number')
 const operatorButtons = document.querySelectorAll('.operator');
 
-const equalsButton = document.getElementById('#equals');
-const deleteButton = document.getElementById('#delete');
-const allClearButton = document.getElementById('#allclear');
+const equalsButton = document.getElementById('equals');
+const deleteButton = document.getElementById('delete');
+const allClearButton = document.getElementById('allclear');
 
-const previousOperandText = document.getElementById('#history-value');
-const currentOperandText = document.getElementById('#output-value');
+const previousOperandText = document.getElementById('history')
+const currentOperandText = document.getElementById('output')
 
 // Creating an object for the class
 const calculator = new Calculator(previousOperandText, currentOperandText)
@@ -137,7 +148,7 @@ operatorButtons.forEach((button) => {
   })
 })
 
-equalsButton.addEventListener("click", button => { 
+equalsButton.addEventListener('click', button => { 
   calculator.compute();
   calculator.updateDisplay();
 })
